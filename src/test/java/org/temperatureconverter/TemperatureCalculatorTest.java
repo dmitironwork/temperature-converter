585044fe018562c3a3ce2e457edfba3df731a4d7
Dmytro Beznosov <dmitironwork@gmail.com>
2026-08-02 15:11:18 +0200

    Add new tests for Celsius To Fahrenhei and Data provider

--- src/test/java/org/temperatureconverter/TemperatureCalculatorTest.java
+++ src/test/java/org/temperatureconverter/TemperatureCalculatorTest.java
@@ -1,16 +1,38 @@
 package org.temperatureconverter;
 
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
 
 public class TemperatureCalculatorTest {
 
+    @DataProvider(name = "celsiusToFahrenheitData")
+    public Object[][] celsiusToFahrenheitData() {
+        return new Object[][] {
+                {-40.0, -40.0},
+                {37.0, 98.6},
+                {-273.15, -459.67}
+        };
+    }
+
+    @Test(dataProvider = "celsiusToFahrenheitData")
+    public void testCelsiusToFahrenheit_variousValues(double celsius, double expectedFahrenheit) {
+        double result = TemperatureCalculator.celsiusToFahrenheit(celsius);
+        Assert.assertTrue(TestDataHelper.isCloseEnough(result, expectedFahrenheit));
+    }
+
     @Test
-    public void testCelsiumFarenheit_freezingPoin() {
+    public void testCelsiusToFarenheit_freezingPoin() {
         double result = TemperatureCalculator.celsiusToFahrenheit(0.0);
         Assert.assertTrue(TestDataHelper.isCloseEnough(result, 32.0));
+    }
+
+    @Test
+    public void testCelsiusToFahrenheit_boilingPoint() {
+        double result = TemperatureCalculator.celsiusToFahrenheit(100);
+        Assert.assertTrue(TestDataHelper.isCloseEnough(result, 212.0));
     }
 
     @BeforeMethod
