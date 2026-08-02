dd7813f849ab3e8f9fd4b3b4e9734b686503d46d
Dmytro Beznosov <dmitironwork@gmail.com>
2026-08-02 14:44:17 +0200

    Add Before After in test

--- src/test/java/org/temperatureconverter/TemperatureCalculatorTest.java
+++ src/test/java/org/temperatureconverter/TemperatureCalculatorTest.java
@@ -2,6 +2,8 @@ package org.temperatureconverter;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
 
 public class TemperatureCalculatorTest {
 
@@ -9,5 +11,15 @@ public class TemperatureCalculatorTest {
     public void testCelsiumFarenheit_freezingPoin() {
         double result = TemperatureCalculator.celsiusToFahrenheit(0.0);
         Assert.assertTrue(TestDataHelper.isCloseEnough(result, 32.0));
+    }
+
+    @BeforeMethod
+    public void setUp() {
+        System.out.println("Start test...");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        System.out.println("Test finished.\n");
     }
 }
