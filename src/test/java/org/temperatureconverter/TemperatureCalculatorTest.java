122de93c517f06413b5ff040ba3cbbc87924aa02
Dmytro Beznosov <dmitironwork@gmail.com>
2026-08-02 13:59:00 +0200

    Fix typo in test method name and restore correct expected value

--- src/test/java/org/temperatureconverter/TemperatureCalculatorTest.java
+++ src/test/java/org/temperatureconverter/TemperatureCalculatorTest.java
@@ -1,4 +1,13 @@
 package org.temperatureconverter;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class TemperatureCalculatorTest {
+
+    @Test
+    public void testCelsiumFarenheit_freezingPoin() {
+        double result = TemperatureCalculator.celsiusToFahrenheit(0.0);
+        Assert.assertTrue(TestDataHelper.isCloseEnough(result, 32.0));
+    }
 }
--- src/test/java/org/temperatureconverter/TestDataHelper.java
+++ src/test/java/org/temperatureconverter/TestDataHelper.java
@@ -1,5 +1,9 @@
 package org.temperatureconverter;
 
 public class TestDataHelper {
+    public static final double DELTA = 0.001;
 
+    public static boolean isCloseEnough(double actual, double expected) {
+        return Math.abs(actual - expected) < DELTA;
+    }
 }
